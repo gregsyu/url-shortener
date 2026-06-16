41b8d0d61f23be391450fc2af0c806e7110e247c
gregsyu <greg3@tutamail.com>
2026-06-16 10:48:39 -0300

    feat: add `UserPayload` type to `User` decorator

--- src/auth/decorator/user.decorator.ts
+++ src/auth/decorator/user.decorator.ts
@@ -1,6 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { UserPayload } from '../../url/dto/url.dto';
 
-export const User = createParamDecorator((_, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest();
-  return request.user;
-});
+export const User = createParamDecorator(
+  (_, ctx: ExecutionContext): UserPayload => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
